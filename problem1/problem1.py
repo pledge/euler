b7603df6fe7cd3d290f7eceea1526661be85d19e
Martin Gilday <mgilday@imap.cc>
2012-01-25 22:07:01 +0000

    Problem 1 using list comprehension

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -2,9 +2,21 @@
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = 0
+# basic
+
+answer = 233168
+
+result = 0
 for i in xrange(1000):
 	if not i % 3 or not i % 5:
-		sum += i
+		result += i
 
-print sum
+assert result == answer
+
+# using sum
+assert answer == sum(filter(lambda x: not x % 3 or not x % 5, xrange(1000)))
+
+# using list comprehension
+assert answer == sum([x for x in xrange(1000) if x % 3 == 0 or x % 5 == 0])
+
+print 'done'
