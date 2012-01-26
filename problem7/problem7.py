2f4dd770d3fe72d223ddde70b3049d84640bfb28
Martin Gilday <mgilday@imap.cc>
2012-01-26 20:58:12 +0000

    Slightly improved problem 7

--- problem7/problem7.py
+++ problem7/problem7.py
@@ -1,14 +1,17 @@
-# using trial division of integers between 2 and sqrt n
-# starting with lower more probable numbers
+# Using trial division of integers between 2 and sqrt n
+# Starting with lower more probable numbers
+#
+# Only checks odd numbers
+# `primes` counter starts at 1 to include '2' as the first prime
 
 from math import sqrt
 
 target = 10001
-primes = 0
+primes = 1 
 n = 1
 
 while primes < target:
-	n += 1
+	n += 2
 	found = False
 	upper = int(sqrt(n)) + 1
 	for i in xrange(2, upper):
